ad4a930a7dfa67abf660cba57875cc76fd9dab59
KeishiroNagaseForMacBookPro <keit0728.job@gmail.com>
2023-01-08 02:07:48 +0900

    upload to github

--- pages/index.tsx
+++ pages/index.tsx
@@ -209,7 +209,7 @@ const Home: NextPage = () => {
     return (
       <div className={styles.container}>
         <main className={styles.main}>
-          <h1 className={styles.title}>Welcome to Tokyo Sauna Collective !!</h1>
+          <h1 className={styles.title}>Welcome to keit DAO Collective !!</h1>
           <div className={styles.connect}>
             <ConnectWallet />
           </div>
